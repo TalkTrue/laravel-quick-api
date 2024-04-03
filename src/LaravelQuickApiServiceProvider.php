79237bd55273fcea79268c25bedde6368a16a0fe
timenow <705219520@qq.com>
2024-04-03 15:48:50 +0800

    1

---
+++ src/LaravelQuickApiServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Talktrue\LaravelQuickApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelQuickApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        var_dump(1);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
